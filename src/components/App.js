ac4b543b0cbe9cd64b7f4052cee9daa85906388f
adityatadas24 <adityatadas2000@gmail.com>
2023-12-18 23:15:39 +0530

    submitting code-Mon Dec 18 2023 23:15:37 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,67 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+{/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
+import React, { useState, useMemo } from 'react';
 
+
+const TodoList = ({ todos }) => {
+    return (
+      <div>
+        <h2>Todo List</h2>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  const App = () => {
+    const [count, setCount] = useState(0);
+    const [newTodo, setNewTodo] = useState('');
+    const [todos, setTodos] = useState([]);
+  
+    const handleAddTodo = () => {
+      setTodos([...todos, 'New todo']);
+    };
+  
+    const handleIncrement = () => {
+      setCount(count + 1);
+    };
+  
+    const handleInputChange = (e) => {
+      setNewTodo(e.target.value);
+    };
+  
+    const handleSubmit = () => {
+      if (newTodo.length > 5) {
+        setTodos([...todos, newTodo]);
+        setNewTodo('');
+      }
+    };
+  
+    // Memoized component
+    const MemoizedTodoList = useMemo(() => <TodoList todos={todos} />, [todos]);
+  
+    return (
+      <div>
+        <h1>Memo App</h1>
+        <div>
+          <button onClick={handleAddTodo}>Add Todo</button>
+          <button onClick={handleIncrement}>Increment Count: {count}</button>
+        </div>
+        {MemoizedTodoList}
+        <div>
+          <h2>Add Todo</h2>
+          <input type="text" value={newTodo} onChange={handleInputChange} />
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+      </div>
+    );
+  };
+  
+  
+  
+  
+  
+  export default App;
+  
